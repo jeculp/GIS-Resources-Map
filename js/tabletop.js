c8e65ade8206779984c6221e3aed9bee5bb26831
Rex Feng <rexfeng@gmail.com>
2015-02-06 18:17:12 -0800

    organize vendor js into vendor dir and load app js logic on DOM ready

--- js/tabletop.js
+++
@@ -1,9 +0,0 @@
-  console.log("test")
-  window.onload = function() { init() };
-  var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0AgDW4THnpFhkdExhY1hmeXpGc25CYXlOenRGVzZ6YUE&output=html';
-
-  function init() {
-    Tabletop.init( { key: public_spreadsheet_url,
-                     callback: function(data, tabletop) { console.log(data) },
-                     simpleSheet: true } );
-  }
