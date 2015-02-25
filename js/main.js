c87c66f4ed1d7c3b66fd0173d7e9ac6988d5c3db
csterling <chandlersterling1@gmail.com>
2015-02-25 15:49:35 -0800

    move map stuff into its own function, call that func from $doc.ready

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
                 processData(data);
                 createSearchHandler();
                 setNavbarHandlers();
+                mapInit();
             }
          });
     }
@@ -165,94 +166,93 @@ $(document).ready(function() {
         map.addLayer(citysim);
     }
 
-    //This loads the map
-    var map = L.map('map')
-        .setView([36.745487, -119.553223], 6);
-    map.options.minZoom = 6;
-    var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-    }).addTo(map).setOpacity(.75);
-
-    var countylines = {
-        "clickable": true,
-        "color": "#47a3da",
-        "fill": false,
-        "dashArray": '3',
-        "weight": .5,
-        "opacity": 1,
-        "fillOpacity": 0.2
-    };
-
-    var countysim = new L.geoJson.ajax("data/countysimple.geojson", {
-        style: countylines,
-    }).addTo(map);
-
-        
-    
-    //Gets and returns colors for Cities that have a web page link in geojson file
+        //Gets and returns colors for Cities that have a web page link in geojson file
     function getcitycolor(d) {
         var d = String(d);
         return d == 'null' ? '#C26263' :
             '#47a3da';
     }
-
-    var markerMap = {}; //Creates marker array to match with list ids
     
-    var markerlayer = L.layerGroup().addTo(map);
-    
-    //Adds a layer with Incorporated Cities onto map, styling performed within
-    var citysim = new L.geoJson.ajax("data/cities.geojson", {
-        pointToLayer: function(feature, latlng) {
-            
-            var marker = new L.circleMarker(latlng, {
-                radius: 3,
-                color: '#bb4c3c',
-                weight: 0.0,
-                fillColor: getcitycolor(feature.properties["GIS Page"]), //this passes an attribute from the json file to a function to return a specified color
-                fillOpacity: .7
-            }).bindPopup("<b>City:</b> " + feature.properties.NAMELSAD + "<br> " +
-                "<b>Name:</b> " + feature.properties["First Name"] + " " + feature.properties["Last Name"] + "<br> " +
-                "<b>Title:</b> " + feature.properties["Title"] + "<br> " +
-                "<b>Agency:</b> " + feature.properties["Agency"] + "<br> " +
-                "<b>email:</b> " + feature.properties["e-mail"] + "<br> " +
-                "<b>Phone:</b> " + feature.properties["Phone"] + "<br> " +
-                "<b>GIS Page:</b> " + '<a href="' + feature.properties["GIS Page"] + '">Link</a>');
-            markerMap[feature.properties.NAMELSAD] = marker;
-            return marker;
-        }
-    }).addTo(markerlayer);
-    
-    
-    
-    var info = L.control();
+    function mapInit(){
+        //This loads the map
+        var map = L.map('map')
+            .setView([36.745487, -119.553223], 6);
+        map.options.minZoom = 6;
+        var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+        }).addTo(map).setOpacity(.75);
 
-    info.onAdd = function(map) {
-        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-        this.update();
-        return this._div;
-    };
+        var countylines = {
+            "clickable": true,
+            "color": "#47a3da",
+            "fill": false,
+            "dashArray": '3',
+            "weight": .5,
+            "opacity": 1,
+            "fillOpacity": 0.2
+        };
 
-    // method that we will use to update the control based on feature properties passed
-    info.update = function(props) {
-        this._div.innerHTML = '<h4>Legend</h4><i style="background:#bb4c3c"></i><h4>City has no website</h4><i style="background:#47a3da"></i><h4>City has website</h4><br><h4>Click on topleft to search for a city!</h4>';
-    };
+        var countysim = new L.geoJson.ajax("data/countysimple.geojson", {
+            style: countylines,
+        }).addTo(map);
 
-    info.addTo(map);
+        var markerMap = {}; //Creates marker array to match with list ids
+        
+        var markerlayer = L.layerGroup().addTo(map);
+        
+        //Adds a layer with Incorporated Cities onto map, styling performed within
+        var citysim = new L.geoJson.ajax("data/cities.geojson", {
+            pointToLayer: function(feature, latlng) {
+                
+                var marker = new L.circleMarker(latlng, {
+                    radius: 3,
+                    color: '#bb4c3c',
+                    weight: 0.0,
+                    fillColor: getcitycolor(feature.properties["GIS Page"]), //this passes an attribute from the json file to a function to return a specified color
+                    fillOpacity: .7
+                }).bindPopup("<b>City:</b> " + feature.properties.NAMELSAD + "<br> " +
+                    "<b>Name:</b> " + feature.properties["First Name"] + " " + feature.properties["Last Name"] + "<br> " +
+                    "<b>Title:</b> " + feature.properties["Title"] + "<br> " +
+                    "<b>Agency:</b> " + feature.properties["Agency"] + "<br> " +
+                    "<b>email:</b> " + feature.properties["e-mail"] + "<br> " +
+                    "<b>Phone:</b> " + feature.properties["Phone"] + "<br> " +
+                    "<b>GIS Page:</b> " + '<a href="' + feature.properties["GIS Page"] + '">Link</a>');
+                markerMap[feature.properties.NAMELSAD] = marker;
+                return marker;
+            }
+        }).addTo(markerlayer);    
+        
+        var info = L.control();
 
-    // expand lists
-    $(".list-title").click(function () {
-        // gets bit group from id
-        var group = $(this).attr("id").substring(0,$(this).attr("id").indexOf("-"));
-        // check if it already has visible class
-        if ($("#"+group+"-list").hasClass("visible-list")) {
-            $("#"+group+"-list").removeClass("visible-list")
-        } else {
-            // remove visible class from all
-            $(".group-list").removeClass("visible-list");
-            // add the class to the selected one
-            $("#"+group+"-list").addClass("visible-list");
-        }
-    });
+        info.onAdd = function(map) {
+            this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+            this.update();
+            return this._div;
+        };
+
+        // method that we will use to update the control based on feature properties passed
+        info.update = function(props) {
+            this._div.innerHTML = '<h4>Legend</h4><i style="background:#bb4c3c"></i><h4>City has no website</h4><i style="background:#47a3da"></i><h4>City has website</h4><br><h4>Click on topleft to search for a city!</h4>';
+        };
+
+        info.addTo(map);
+
+        // expand lists
+        $(".list-title").click(function () {
+            // gets bit group from id
+            var group = $(this).attr("id").substring(0,$(this).attr("id").indexOf("-"));
+            // check if it already has visible class
+            if ($("#"+group+"-list").hasClass("visible-list")) {
+                $("#"+group+"-list").removeClass("visible-list")
+            } else {
+                // remove visible class from all
+                $(".group-list").removeClass("visible-list");
+                // add the class to the selected one
+                $("#"+group+"-list").addClass("visible-list");
+            }
+        });
+
+    }
 
     function createSearchHandler() {
         $( "form.search" ).submit(function( event ) {
